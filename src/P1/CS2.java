20bab869661e99e9fa57f0a4e91dc8847261e7c3
Its-Shubham-0246 <shubhammaurya2406@gmail.com>
2025-08-26 19:02:52 +0530

    Initial commit

---
+++ src/P1/CS2.java
@@ -0,0 +1,14 @@
+package P1;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CS2 {
+    public static void main(String[] args) throws FileNotFoundException{
+        System.out.println("New Main");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter here : ");
+        String data = sc.next();
+        System.out.println(data);
+
+    }
+}
